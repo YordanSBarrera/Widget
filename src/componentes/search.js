dacd707832494615dc088db639b21305e7813d93
YORDAN <yordansaldanabarrera@gmail.com>
2021-01-10 21:07:04 -0300

    arreglando las solicitud de busqueda

--- src/componentes/search.js
+++ src/componentes/search.js
@@ -2,23 +2,61 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const Search = () => {
-    const [term, setTerm] = useState('');
+    const [term, setTerm] = useState('Yordan');
+    const [debouncedTerm, setDebouncedTerm] = useState(term);
+    const [results, setResult] = useState([]);
 
-    console.log('prueba de gancho')
     useEffect(() => {
-        const resp = async () => {
-            await axios.get('https://en.wikipedia.org/w/api.php', {
+        const timerId = setTimeout(() => {
+            setDebouncedTerm(term);
+        }, 1000);
+        return () => {
+            clearTimeout(timerId);
+        }
+    }, [term]);
+
+    useEffect(() => {
+        const search = async () => {
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
                 params: {
                     action: 'query',
                     list: 'search',
                     origin: '*',
                     format: 'json',
-                    srsearch: term,
+                    srsearch: debouncedTerm,
                 },
             });
+            setResult(data.query.search);
         };
-        console.log(resp)
-    }, )
+        search();
+    }, [debouncedTerm])
+
+
+    const renderResults = results.map((result) => {
+        return (
+            <div className="item" key={result.pageid}>
+                <div className="right floated content">
+                    <a
+                        className="ui button"
+                        href={`https://en.wikipedia.org?curid=${result.pageid}`}
+                    >
+                        Go
+                        </a>
+                </div>
+                <div className="content">
+                    <div className="header">
+                        {result.title}
+                    </div>
+                    <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+                    <div className="foot">
+                        <br />
+                        conteo {result.wordcount}
+                    </div>
+                </div>
+            </div>
+        );
+    });
+
     return (
         <div >
             <div className="ui form">
@@ -31,6 +69,10 @@ const Search = () => {
                         onChange={(e) => { setTerm(e.target.value) }}
                     />
                 </div>
+            </div>
+            <br />
+            <div className="ui celled list">
+                {renderResults}
             </div>
         </div>
     )
